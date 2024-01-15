7b1e8a87efeeb2ce8fff4b08aa7cfb3c77bdf246
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2024-01-15 20:51:39 -0300

    criando crud de paciente

--- api/src/main/java/med/clim/api/controller/MedicoController.java
+++ api/src/main/java/med/clim/api/controller/MedicoController.java
@@ -9,11 +9,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("medicos")
-public class medicoController {
+public class MedicoController {
     @Autowired
     private MedicoRepository repository;
     @PostMapping
